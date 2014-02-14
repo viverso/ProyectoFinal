b0e845e95c5af499e54c6bb82b89db1336dc3b87
Gildardo <Lanix 08>
2014-02-14 12:15:47 -0600

    Cambios del beep
    Se agrego la funcionalidad del beep

--- js/acciones.js
+++ js/acciones.js
@@ -5,5 +5,10 @@ $(document).ready(function(){
 		$('#b1').tap(function(){
 			navigator.notification.beep(1);
 		});//Cierre de tap b1
+		
+		$('#b2').tap(function(){
+			navigator.notification.vibrate(500);
+		});//Cierre de tap b2
+		
 	}); //Cierre de deviceready
 });// Cierre de ready
